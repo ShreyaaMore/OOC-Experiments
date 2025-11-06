38222c69556add6ea0f09da605d198aa441d4160
Shreya More <shreyaamore21@gmail.com>
2025-11-07 00:31:23 +0530

    Create Exp1.cpp

---
+++ Exp1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+class Student {
+private:
+    char name[30];  
+    int rollNo;
+    float marks;
+
+public:
+   
+    void inputDetails() {
+        cout << "Enter student name: ";
+        cin >> name;
+        cout << "Enter roll number: ";
+        cin >> rollNo;
+        cout << "Enter marks: ";
+        cin >> marks;
+    }
+
+    void displayDetails() {
+        cout << "Student Name: " << name << endl;
+        cout << "Roll Number: " << rollNo << endl;
+        cout << "Marks: " << marks << endl;
+    }
+};
+
+int main() {
+    Student studentObj;
+
+    studentObj.inputDetails();
+
+    cout << "\nStudent Details:\n";
+    studentObj.displayDetails();
+
+    return 0;
+}
